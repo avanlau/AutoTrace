4ead57d0d64da10a4fa9fb9552e2b5dd63ecaf3e
Christer Eriksson <christer@dogtail.se>
2016-07-03 23:46:41 +0200

    Converted to xUnit insted if MSTest

---
+++ AutoTrace.UnitTests/GetMethodDelcarationTestsxUnit.cs
@@ -0,0 +1,92 @@
+﻿using AutoTrace.Generator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoTrace.UnitTests
+{
+    public enum TypeKind
+    {
+        Class,
+        Struct
+    }
+
+    [Collection("TextCode")]
+    public class GetMethodDelcarationTestsxUnit
+    {
+        TextCodeFixture fixture;
+
+        public GetMethodDelcarationTestsxUnit(TextCodeFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Theory]
+        [InlineData(TypeKind.Class, "MethodWithoutReturnValueAndParameters", "public void MethodWithoutReturnValueAndParameters()")]
+        [InlineData(TypeKind.Class, "MethodWithReturnValueWithoutParameters", "public Int32 MethodWithReturnValueWithoutParameters()")]
+        [InlineData(TypeKind.Class, "MethodWithoutReturnValueWithParameters", "public void MethodWithoutReturnValueWithParameters(Int32 a, String b)")]
+        [InlineData(TypeKind.Class, "MethodWithReturnValueAndParameters", "public String MethodWithReturnValueAndParameters(Int32 a, String b)")]
+        [InlineData(TypeKind.Struct, "MethodWithoutReturnValueAndParameters", "public void MethodWithoutReturnValueAndParameters()")]
+        [InlineData(TypeKind.Struct, "MethodWithReturnValueWithoutParameters", "public Int32 MethodWithReturnValueWithoutParameters()")]
+        [InlineData(TypeKind.Struct, "MethodWithoutReturnValueWithParameters", "public void MethodWithoutReturnValueWithParameters(Int32 a, String b)")]
+        [InlineData(TypeKind.Struct, "MethodWithReturnValueAndParameters", "public String MethodWithReturnValueAndParameters(Int32 a, String b)")]
+        public void GetMethodDeclarationTests(TypeKind typeKind, string methodName, string expected)
+        {
+            // Arange
+            IMethodSymbol methodSymbol = GetMetodSymbol(typeKind, methodName);
+
+            // Act
+            var actual = methodSymbol.GetMethodDeclaration();
+
+            // Assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(TypeKind.Class, "MethodWithoutReturnValueAndParameters", "public void TestNamespace.TestClass.MethodWithoutReturnValueAndParameters()")]
+        [InlineData(TypeKind.Class, "MethodWithReturnValueWithoutParameters", "public Int32 TestNamespace.TestClass.MethodWithReturnValueWithoutParameters()")]
+        [InlineData(TypeKind.Class, "MethodWithoutReturnValueWithParameters", "public void TestNamespace.TestClass.MethodWithoutReturnValueWithParameters(Int32 a, String b)")]
+        [InlineData(TypeKind.Class, "MethodWithReturnValueAndParameters", "public String TestNamespace.TestClass.MethodWithReturnValueAndParameters(Int32 a, String b)")]
+        [InlineData(TypeKind.Struct, "MethodWithoutReturnValueAndParameters", "public void TestNamespace.TestStruct.MethodWithoutReturnValueAndParameters()")]
+        [InlineData(TypeKind.Struct, "MethodWithReturnValueWithoutParameters", "public Int32 TestNamespace.TestStruct.MethodWithReturnValueWithoutParameters()")]
+        [InlineData(TypeKind.Struct, "MethodWithoutReturnValueWithParameters", "public void TestNamespace.TestStruct.MethodWithoutReturnValueWithParameters(Int32 a, String b)")]
+        [InlineData(TypeKind.Struct, "MethodWithReturnValueAndParameters", "public String TestNamespace.TestStruct.MethodWithReturnValueAndParameters(Int32 a, String b)")]
+        public void GetMethodDelcarationWithNamespace(TypeKind typeKind, string methodName, string expected)
+        {
+            // Arange
+            IMethodSymbol methodSymbol = GetMetodSymbol(typeKind, methodName);
+
+            // Act
+            var actual = methodSymbol.GetMethodDeclarationWithNamespace();
+
+            // Assert;
+            Assert.Equal(expected, actual);
+        }
+
+        private IMethodSymbol GetMetodSymbol(TypeKind typeKind, string methodName)
+        {
+            INamedTypeSymbol symbol;
+            switch (typeKind)
+            {
+                case (TypeKind.Class):
+                    symbol = fixture.TestClassSymbol;
+                    break;
+                case (TypeKind.Struct):
+                    symbol = fixture.TestStructSymbol;
+                    break;
+                default:
+                    throw new NullReferenceException();
+            }
+
+            var methodSymbol = (IMethodSymbol)symbol.GetMembers().Where(m => m.Name.Equals(methodName)).First();
+
+            return methodSymbol;
+        }
+    }
+}
---
+++ AutoTrace.UnitTests/TextCodeFixture.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoTrace.UnitTests
+{
+    public class TextCodeFixture : IDisposable
+    {
+        public TextCodeFixture()
+        {
+            Source = File.ReadAllText("TestCode.cs");
+            var tree = CSharpSyntaxTree.ParseText(Source);
+
+            var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            var compilation = CSharpCompilation.Create("MyCompilation", syntaxTrees: new[] { tree }, references: new[] { mscorlib });
+            TestClassSymbol = compilation.GetTypeByMetadataName("TestNamespace.TestClass");
+            TestStructSymbol = compilation.GetTypeByMetadataName("TestNamespace.TestStruct");
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public string Source { get; set; }
+        public INamedTypeSymbol TestClassSymbol { get; private set; }
+        public INamedTypeSymbol TestStructSymbol { get; private set; }
+
+    }
+
+    [CollectionDefinition("TextCode")]
+    public class DatabaseCollection : ICollectionFixture<TextCodeFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
